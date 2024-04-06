a9f00399cc1631208af652cb05d0f886e5a63775
Vlad Mironov <vlad372@yandex.ru>
2024-04-06 13:10:25 +0300

    Добавил кнопки да и нет

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -5,42 +5,14 @@ final class MovieQuizViewController:
                                         
     UIViewController {
     
-    @IBOutlet weak var labelTest: UILabel!
+    
     @IBOutlet weak var buttonYes: UIButton!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        func printAllAvailableFonts() {
-            let familyNames = UIFont.familyNames.sorted()
-            
-            for familyName in familyNames {
-                print("Family: \(familyName)")
-                
-                let fontNames = UIFont.fontNames(forFamilyName: familyName)
-                for fontName in fontNames {
-                    print("  Font: \(fontName)")
-                }
-            }
-        }
-
-        // Вызовите функцию, чтобы распечатать все доступные шрифты
-        printAllAvailableFonts()
-        
-        labelTest.text = "Бла-бла бла"
-        labelTest.font = UIFont(name: "YSDisplay-Medium", size: 20)
-        buttonYes.setTitleColor(UIColor.ypBlack, for: .normal)
-        buttonYes.tintColor = UIColor.ypGray
-        buttonYes.frame.size.width = 156
-        buttonYes.frame.size.height = 60
-        buttonYes.contentVerticalAlignment = .center
-        buttonYes.contentHorizontalAlignment = .center
-        buttonYes.setTitle("Да", for: .normal)
-        buttonYes.subtitleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
-        DispatchQueue.main.async {
-            self.buttonYes.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
-        }
+        buttonYes.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
     }
 }
 
