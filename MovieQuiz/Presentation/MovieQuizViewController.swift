7959092ddbfe2b31e305fa2108fa148e0f4ffd10
Vlad Mironov <vlad372@yandex.ru>
2024-04-10 13:03:50 +0300

    Попытка 2 сдачи

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -53,20 +53,26 @@ final class MovieQuizViewController:
         let correctAnswer: Bool
     }
     
+    //MARK: - Стркутура данных для запросов.
+    
+    private struct Questions {
+        static let testQuestions: [QuizQuestion] = [QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+                                                QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+                                                QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+                                                QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+                                                QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+                                                QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+                                                QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+                                                QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+                                                QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+                                                QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+                                                
+       ]
+    }
+    
     //MARK: - Моковые данные.
     
-    private let questions: [QuizQuestion] = [QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-                                             QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-                                             QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-                                             QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-                                             QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-                                             QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-                                             QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-                                             QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-                                             QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-                                             QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-                                             
-    ]
+    private let questions: [QuizQuestion] = Questions.testQuestions
     
     //MARK: - Метод конвертации.
     //Принимает моковый вопрос и возвращает вью модель для экрана вопроса
@@ -104,30 +110,35 @@ final class MovieQuizViewController:
         }
     }
     
+    //MARK: - Резултат квиза
+    
+    private func showResultQuiz(){
+        let alert = UIAlertController(title: "Этот раунд окончен!", // заголовок всплывающего окна
+                                      message: "Ваш результат: \(correctAnswer)/\(questions.count)", // текст во всплывающем окне
+                                      preferredStyle: .alert) // preferredStyle может быть .alert или .actionSheet
+        
+        // создаём для алерта кнопку с действием
+        // в замыкании пишем, что должно происходить при нажатии на кнопку
+        let action = UIAlertAction(title: "Сыграть еще раз", style: .default) { _ in
+            print("OK button is clicked!")
+            self.currentQuestionIndex = 0
+            self.correctAnswer = 0
+            self.show(quiz: self.convert(model: self.questions[0]))
+        }
+        
+        // добавляем в алерт кнопку
+        alert.addAction(action)
+        
+        // показываем всплывающее окно
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     //MARK: - Переход в один из сценариев.
     
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 { // 1
             // идём в состояние "Результат квиза"
-            // создаём объекты всплывающего окна
-            let alert = UIAlertController(title: "Этот раунд окончен!", // заголовок всплывающего окна
-                                          message: "Ваш результат: \(correctAnswer)/\(questions.count)", // текст во всплывающем окне
-                                          preferredStyle: .alert) // preferredStyle может быть .alert или .actionSheet
-            
-            // создаём для алерта кнопку с действием
-            // в замыкании пишем, что должно происходить при нажатии на кнопку
-            let action = UIAlertAction(title: "Сыграть еще раз", style: .default) { _ in
-                print("OK button is clicked!")
-                self.currentQuestionIndex = 0
-                self.correctAnswer = 0
-                self.show(quiz: self.convert(model: self.questions[0]))
-            }
-            
-            // добавляем в алерт кнопку
-            alert.addAction(action)
-            
-            // показываем всплывающее окно
-            self.present(alert, animated: true, completion: nil)
+            showResultQuiz()
         } else { // 2
             // идём в состояние "Вопрос показан"
             currentQuestionIndex += 1
