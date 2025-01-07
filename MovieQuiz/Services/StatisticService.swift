2c4ab9ea4f37b8b3984d585664b845d899185c8c
Vlad Mironov <vlad372@yandex.ru>
2025-01-07 16:06:54 +0300

    Сдача - попытка 1

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -6,16 +6,16 @@ final class StatisticService: StatisticServiceProtocol {
         case correct
         case bestGame
         case gamesCount
-
+        
         case bestGameDate
         case bestGameCorrect
         case bestGameTotal
-
+        
         case totalCorrectAnswers
         case totalAnwsers
         case totalAccuracy
     }
-
+    
     var gamesCount: Int {
         get {
             storage.integer(forKey: Keys.gamesCount.rawValue)
@@ -24,7 +24,7 @@ final class StatisticService: StatisticServiceProtocol {
             storage.set(newValue, forKey: Keys.gamesCount.rawValue)
         }
     }
-
+    
     var bestGame: GameResult {
         get {
             if let bestGameDate = storage.object(forKey: Keys.bestGameDate.rawValue) as? Date {
@@ -32,7 +32,7 @@ final class StatisticService: StatisticServiceProtocol {
                 let bestGameTotal = storage.integer(forKey: Keys.bestGameTotal.rawValue)
                 return GameResult(correct: bestGameCorrect, total: bestGameTotal, date: bestGameDate)
             }
-
+            
             return GameResult(correct: 0, total: 0, date: Date())
         }
         set {
@@ -41,44 +41,45 @@ final class StatisticService: StatisticServiceProtocol {
             storage.set(newValue.total, forKey: Keys.bestGameTotal.rawValue)
         }
     }
-
+    
     /// отношение всех правильных ответов от общего числа вопросов
     var totalAccuracy: Double {
         get {
             if gamesCount != 0 {
                 print("Пытаюсь получить totalAccuracy")
                 let totalCorrectAnswers = Double(storage.integer(forKey: Keys.totalCorrectAnswers.rawValue))
-
+                
                 let totalAnwsers = Double(storage.integer(forKey: Keys.totalAnwsers.rawValue))
                 //            return Double(totalCorrectAnswers / qtyQuestion * gamesCount * 100)
                 let accurency = Double(totalCorrectAnswers / totalAnwsers * 100)
+                
                 return accurency
             }
-
+            
             return 0
         }
         set {
             storage.set(newValue, forKey: Keys.totalAccuracy.rawValue)
         }
     }
-
+    
     func store(_ gameResult: GameResult) {
         gamesCount += 1
         let currentTotalCorrectAnswers = storage.integer(forKey: Keys.totalCorrectAnswers.rawValue)
         let currentTotalAnwsers = storage.integer(forKey: Keys.totalAnwsers.rawValue)
-
+        
         let updatedtTotalCorrectAnswers: Int = gameResult.correct + currentTotalCorrectAnswers
         let updatedtTotalAnwsers: Int = gameResult.total + currentTotalAnwsers
-
+        
         storage.set(updatedtTotalCorrectAnswers, forKey: Keys.totalCorrectAnswers.rawValue)
         storage.set(updatedtTotalAnwsers, forKey: Keys.totalAnwsers.rawValue)
-
+        
         if gameResult.isBetterThen(bestGame) {
             print("Новый рекорд 🎉")
             bestGame = gameResult
         }
     }
-
+    
     func getAll() {
         print("--- Статистика ---")
         print("Количество игр: \(gamesCount)")
@@ -88,13 +89,13 @@ final class StatisticService: StatisticServiceProtocol {
         print("Всего вопросов: \(storage.integer(forKey: Keys.totalAnwsers.rawValue))")
         print("----------------")
     }
-
+    
     func clearAll() {
         gamesCount = 0
         bestGame.correct = 0
         bestGame.total = 0
         totalAccuracy = 0
-
+        
         storage.set(0, forKey: Keys.totalCorrectAnswers.rawValue)
         storage.set(0, forKey: Keys.totalAnwsers.rawValue)
         print("--- ⚠️ Статистика сброшена ⚠️ ---")
